0c7c2c9c709531e14804d82d347d2c428af4c14e
Nat <hinathanielong@gmail.com>
2020-06-08 09:18:54 +0800

    Changed toggle class to add / remove class to resolve minor issues.

--- js/RegisterLogin.js
+++ js/RegisterLogin.js
@@ -9,14 +9,14 @@ function register() {
   loginForm.css("left", "-400px");
   regForm.style.left = "50px";
   btnBoxBGColor.style.left = "0";
-  registerButton.toggleClass('text-light')
-  loginButton.toggleClass('text-light')
+  registerButton.addClass('text-light')
+  loginButton.removeClass('text-light')
 }
 
 function login() {
   loginForm.css("left", "50px");
   regForm.style.left = "-400px";
   btnBoxBGColor.style.left = "116px";
-  registerButton.toggleClass('text-light')
-  loginButton.toggleClass('text-light')
+  registerButton.removeClass('text-light')
+  loginButton.addClass('text-light')
 }
