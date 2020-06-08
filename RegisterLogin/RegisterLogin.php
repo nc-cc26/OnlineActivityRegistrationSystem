3e9e307be1ccfb5124ac4130b7dbb609f7422f25
Nat <hinathanielong@gmail.com>
2020-06-08 11:16:45 +0800

    Added password specification to register form.

--- RegisterLogin/RegisterLogin.php
+++ RegisterLogin/RegisterLogin.php
@@ -61,6 +61,14 @@
                         <input id="new-pw" name="newPW" type="password" class="input-field" placeholder="Enter Password" required />
                     </div>
 
+                    <div id="message">
+                        <h4 style="font-size: medium;">Password must contain the following:</h4>
+                        <p style="font-size: small;" id="letter" class="invalid">A <b>lowercase</b> letter</p>
+                        <p style="font-size: small;" id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>
+                        <p style="font-size: small;" id="number" class="invalid">A <b>number</b></p>
+                        <p style="font-size: small;" id="length" class="invalid">Minimum <b>8 characters</b></p>
+                    </div>
+
                     <div>
                         <input id="reg-checkbox" type="checkbox" class="checkbox" required />
                         <label for id="reg-checkbox" class="checkbox-text">I agree to the <a href="TermsAndConditions.php">terms & conditions</a>.</label>
@@ -99,6 +107,65 @@
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+
+    <script>
+        var myInput = document.getElementById("new-pw");
+        var letter = document.getElementById("letter");
+        var capital = document.getElementById("capital");
+        var number = document.getElementById("number");
+        var length = document.getElementById("length");
+
+        // When the user clicks on the password field, show the message box
+        myInput.onfocus = function() {
+            document.getElementById("message").style.display = "block";
+        }
+
+        // When the user clicks outside of the password field, hide the message box
+        myInput.onblur = function() {
+            document.getElementById("message").style.display = "none";
+        }
+
+        // When the user starts to type something inside the password field
+        myInput.onkeyup = function() {
+            // Validate lowercase letters
+            var lowerCaseLetters = /[a-z]/g;
+            if (myInput.value.match(lowerCaseLetters)) {
+                letter.classList.remove("invalid");
+                letter.classList.add("valid");
+            } else {
+                letter.classList.remove("valid");
+                letter.classList.add("invalid");
+            }
+            // Validate capital letters
+            var upperCaseLetters = /[A-Z]/g;
+            if (myInput.value.match(upperCaseLetters)) {
+                capital.classList.remove("invalid");
+                capital.classList.add("valid");
+            } else {
+                capital.classList.remove("valid");
+                capital.classList.add("invalid");
+            }
+
+            // Validate numbers
+            var numbers = /[0-9]/g;
+            if (myInput.value.match(numbers)) {
+                number.classList.remove("invalid");
+                number.classList.add("valid");
+            } else {
+                number.classList.remove("valid");
+                number.classList.add("invalid");
+            }
+
+            // Validate length
+            if (myInput.value.length >= 8) {
+                length.classList.remove("invalid");
+                length.classList.add("valid");
+            } else {
+                length.classList.remove("valid");
+                length.classList.add("invalid");
+            }
+        }
+    </script>
 
     <script src="../js/RegisterLogin.js"></script>
 </body>
