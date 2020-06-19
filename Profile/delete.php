7042f79c5ba35a3fc2caa2b937ec8508002a45fd
nc-cc26 <62829095+nc-cc26@users.noreply.github.com>
2020-06-19 05:27:17 -0700

    delete data in activitytable

--- Profile/delete.php
+++ Profile/delete.php
@@ -12,16 +12,19 @@ if(isset($_SESSION['logged_in']) && $_SESSION['user_id']
         $sql1 = "DELETE FROM `academictable` WHERE `ID`='$ID'";
         $sql2 = "DELETE FROM `contacttable` WHERE `ID`='$ID'";
         $sql3 = "DELETE FROM `personaltable` WHERE `ID`='$ID'";
+        $sql4 = "DELETE FROM `activitytable` WHERE `ID`='$ID'";
         try{
             $stmt0 = $pdo->prepare($sql0);
             $stmt1 = $pdo->prepare($sql1);
             $stmt2 = $pdo->prepare($sql2);
             $stmt3 = $pdo->prepare($sql3);
-
+            $stmt4 = $pdo->prepare($sql4);
+            
             $stmt0->execute();
             $stmt1->execute();
             $stmt2->execute();
             $stmt3->execute();
+            $stmt4->execute();
             header('Location:deletedAccount.php');
         } catch (Exception $e) {
             echo "Error: " . $e;
